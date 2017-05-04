f6ab05e3b54051433abbe8f54c791ce0878defa1
xusen <956532577@qq.com>
2017-05-04 22:11:25 +0800

    Test

---
+++ app/src/main/java/com/example/line/line/Service/mService.java
@@ -0,0 +1,18 @@
+package com.example.line.line.Service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by xusen on 2017/5/4.
+ */
+
+public class mService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
